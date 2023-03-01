7ab854bd9ff256d22940226e4590128b48116d02
paolo <paolo@furbo.ch>
2023-03-01 17:33:53 +0100

    bla

--- src/RentmanForCraft.php
+++ src/RentmanForCraft.php
@@ -30,8 +30,7 @@ use furbo\rentmanforcraft\variables\RentmanForCraftVariable;
 use furbo\rentmanforcraft\web\assets\rentmanforcraft\RentmanForCraftCPAsset;
 use yii\base\Event;
 
-use craft\web\User;
-use craft\web\Session;
+
 
 /**
  * Rentman for Craft plugin
@@ -198,18 +197,6 @@ class RentmanForCraft extends Plugin
                 }
             }
         );
-
-
-        Event::on(User::class, User::EVENT_AFTER_LOGIN, function(Event $event) {
-            // Get the current session
-            $session = Craft::$app->getSession();
-            // Get the current session ID
-            $sessionId = $session->getId();
-            // Set the session ID as a cookie
-            setcookie('CraftSessionId', $sessionId, 0, '/');
-        });
-
-
 
 
     }
