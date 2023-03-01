0248423c0748a3bf0f7783e33580c41595428475
paolo <paolo@furbo.ch>
2023-03-01 16:35:28 +0100

    keep the guest session id after logging in

--- src/RentmanForCraft.php
+++ src/RentmanForCraft.php
@@ -30,6 +30,8 @@ use furbo\rentmanforcraft\variables\RentmanForCraftVariable;
 use furbo\rentmanforcraft\web\assets\rentmanforcraft\RentmanForCraftCPAsset;
 use yii\base\Event;
 
+use craft\services\UserSessionService; // added by paolo to keep the guest session id after logging in
+
 /**
  * Rentman for Craft plugin
  *
@@ -69,6 +71,9 @@ class RentmanForCraft extends Plugin
         Craft::$app->onInit(function() {
             $this->attachEventHandlers();
             $this->registerLogger();
+        });
+        Event::on(UserSessionService::class, UserSessionService::EVENT_BEFORE_LOGIN, function($event) {
+            $event->loginAttributes['setCookies'] = false;
         });
     }
 
