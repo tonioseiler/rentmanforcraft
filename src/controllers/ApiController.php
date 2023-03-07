4977b60ef709601d535954aa62aa445e7d2c6fab
paolo <paolo@furbo.ch>
2023-03-07 15:54:56 +0100

    actionSubmitProject allow for guests

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -33,6 +33,7 @@ class ApiController extends Controller
         'update-project',
         'set-project-product-quantity',
         'set-project-shooting-days',
+        'actionSubmitProject',
     ];
 
     /**
