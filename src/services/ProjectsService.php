c886606ae41742cab6753bd0fc8ba5f92d9a8d09
paolo <paolo@furbo.ch>
2023-03-08 17:58:57 +0100

    create pdf debug

--- src/services/ProjectsService.php
+++ src/services/ProjectsService.php
@@ -109,22 +109,7 @@ class ProjectsService extends Component
     }
 
     public function generatePDF(Project $project) {
-
-        $view = Craft::$app->getView();
-        $oldTemplateMode = $view->getTemplateMode();
-
-        $view->setTemplateMode($view::TEMPLATE_MODE_SITE);
-        $html = $view->renderTemplate('rentman-for-craft/src/templates/projects/_pdf',['project' => $project]);
-
-        $view->setTemplateMode($oldTemplateMode);
-
-
-
-
-
-
-
-        //$html = Craft::$app->getView()->renderTemplate('rentman-for-craft/projects/_pdf',['project' => $project]);
+        $html = Craft::$app->getView()->renderTemplate('rentman-for-craft/projects/_pdf',['project' => $project]);
        // $html = Craft::$app->getView()->renderTemplate('rentman-for-craft/projects/_pdf',['project' => $project], View::TEMPLATE_MODE_SITE);
 
         $options = new Options();
