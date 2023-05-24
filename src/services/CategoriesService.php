e4b4cc5860e8db162f548e9872a6ed7207386120
paolo <paolo@furbo.ch>
2023-05-24 15:15:56 +0200

    debug

--- src/services/CategoriesService.php
+++ src/services/CategoriesService.php
@@ -28,7 +28,7 @@ class CategoriesService extends Component
 
         $categories = $query->orderBy('order')->all();
         foreach ($categories as $category) {
-            $allCategories[] = $category.id;
+            $allCategories[] = $category->id;
         }
 
         dd($allCategories);
