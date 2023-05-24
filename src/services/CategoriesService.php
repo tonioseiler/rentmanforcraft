dbbea0cefada781f5dce79770785ba2e4ad82a08
paolo <paolo@furbo.ch>
2023-05-24 14:59:39 +0200

    getCategoriesRecursive

--- src/services/CategoriesService.php
+++ src/services/CategoriesService.php
@@ -11,12 +11,31 @@ use yii\base\Component;
  */
 class CategoriesService extends Component
 {
+
+
     public function getCategories($parentId = 0)
     {
         $query = Category::find()
             ->parentId($parentId);
         return $query->orderBy('order')->all();
     }
+
+    public function getCategoriesRecursive($parentId = 0)
+    {
+        $query = Category::find()
+            ->parentId($parentId);
+
+        $categories = $query->orderBy('order')->all();
+
+        foreach ($categories as $category) {
+            $subCategories = $this->getCategories($category->id);
+            $category->setDescendants($subCategories);
+        }
+
+        return $categories;
+    }
+
+
 
     public function getCategoryById($id)
     {
