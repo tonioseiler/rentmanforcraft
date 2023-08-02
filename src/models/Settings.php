142e9a772d4f6ed91ce5899303f1501c92c629c7
paolo <paolo@furbo.ch>
2023-08-02 12:23:19 +0200

    Settings - new test field

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -138,5 +138,6 @@ class Settings extends Model
                                     ['days' => 106, 'factor' => 46],
                                     ['days' => 107, 'factor' => 46.25]
                                 ];
+    public $translationTest = '';
 
 }
