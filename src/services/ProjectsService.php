5af83391a25bc089d70efc872302764cdb0043b6
paolo <paolo@furbo.ch>
2023-08-02 16:47:44 +0200

    template for project pdf - whitelabel version - debug

--- src/services/ProjectsService.php
+++ src/services/ProjectsService.php
@@ -127,7 +127,7 @@ class ProjectsService extends Component
         } else {
             $html = Craft::$app->getView()->renderTemplate($templateToUse,['project' => $project], View::TEMPLATE_MODE_CP);
         }
-        
+        dd('all good till here');
         $options = new Options();
         $options->set('isRemoteEnabled', TRUE);
         $options->set('debugKeepTemp', TRUE);
